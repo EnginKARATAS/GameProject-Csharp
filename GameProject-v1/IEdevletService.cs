a98c022d020d5c4d76642f4792d73fdda6b9bde8
Engin Karataş <enginkaratas99@gmail.com>
2021-02-08 12:26:34 +0300

    IEdevletService added

---
+++ GameProject-v1/IEdevletService.cs
@@ -0,0 +1,13 @@
+﻿namespace GameProject_v1
+{
+    partial class Program
+    {
+        public interface IEdevletService
+        {
+            void TcNo ();   
+            void  A d();
+            void Soyad();
+            void DogumYili();
+        }
+    }
+}
