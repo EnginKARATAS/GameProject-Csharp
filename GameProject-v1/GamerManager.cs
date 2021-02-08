93e85f4502e5fc38623fb28c997fa5fbab95e1c8
Engin Karataş <enginkaratas99@gmail.com>
2021-02-08 16:04:03 +0300

    Good start

---
+++ GameProject-v1/GamerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_v1
+{
+    class GamerManager:IEdevletService,ISatisYap
+    {
+        public void Adduser()
+        {
+            CheckUserFromEdevlet();
+            Console.WriteLine("Game manager ile site kullanıcısı eklendi");
+        }
+
+        public void CheckUserFromEdevlet()
+        {
+            Console.WriteLine("e devletten kullanıcı kontrol edildi");
+        }
+    }
+}
