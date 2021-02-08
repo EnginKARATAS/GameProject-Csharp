78baafd962c3eb726d58046642968cdd1a1c079c
Engin Karataş <enginkaratas99@gmail.com>
2021-02-08 15:53:13 +0300

    Start again

--- GameProject-v1/Program.cs
+++ GameProject-v1/Program.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameProject_v1
 {
-    partial class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            SiteUserManager siteUserManager = new SiteUserManager();
-            //ref alır
-            SiteUser user = new Gamer() { Name="engin",BirthDay="1.1.1",Surname="Karataş",TcNo="1232131"};
-            siteUserManager.AddUser(user); //gamer
-            
+            Console.WriteLine("hi");
+            Console.ReadKey();
         }
     }
 }
- 
